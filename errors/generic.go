6ba48e322f7e54453f9ae90ebb7aea810534b375
Manuel HUEZ <Manu@MacBook-Pro-6.local>
2016-11-18 13:41:51 -0500

    Add error getters to processout errors package

--- errors/generic.go
+++ errors/generic.go
@@ -11,3 +11,13 @@ type Error struct {
 func (e *Error) Error() string {
 	return e.message
 }
+
+// Code returns the error code returned by ProcessOut
+func (e *Error) Code() string {
+	return e.code
+}
+
+// Root returns the root error, if any
+func (e *Error) Root() error {
+	return e.err
+}
