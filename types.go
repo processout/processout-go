e37a12dbbd25a386598dddb5023cdecf2ff5d1ab
Manuel Huez <manuel@Manuels-MacBook-Pro-2.local>
2017-05-04 13:09:15 +0200

    Add pointer helpers

--- types.go
+++ types.go
@@ -26,3 +26,44 @@ func Bool(b bool) *bool {
 func Time(t time.Time) *time.Time {
 	return &t
 }
+
+// ToString returns the value of the string pointer, or an empty string
+func ToString(s *string) string {
+	if s == nil {
+		return ""
+	}
+
+	return *s
+}
+
+// ToInt64 returns the value of the int64 pointer, or 0
+func ToInt64(i *int64) int64 {
+	if i == nil {
+		return 0
+	}
+
+	return *i
+}
+
+// ToFloat64 returns the value of the float64 pointer, or 0
+func ToFloat64(f *float64) float64 {
+	if f == nil {
+		return 0
+	}
+
+	return *f
+}
+
+// ToBool returns the value of the bool pointer, or false
+func ToBool(b *bool) bool {
+	return b != nil && b
+}
+
+// ToTime returns the value of the time pointer, or an empty time.Time struct
+func ToTime(t *time.Time) time.Time {
+	if t == nil {
+		return time.Time{}
+	}
+
+	return *t
+}
