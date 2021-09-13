bf28eddb14664ab9c90073e5a9dc848677be4a3a
ProcessOut Fountain <internal@processout.com>
2021-09-13 08:54:19 +0100

    “fix:

--- three_ds.go
+++ three_ds.go
@@ -15,13 +15,23 @@ import (
 // ThreeDS represents the ThreeDS API object
 type ThreeDS struct {
 	// Version is the version of the 3DS
-	Version *string `json:"Version,omitempty"`
+	Version *string `json:"version,omitempty"`
 	// Status is the current status of the authentication
-	Status *string `json:"Status,omitempty"`
+	Status *string `json:"status,omitempty"`
 	// Fingerprinted is the true if a fingerprint has occured
 	Fingerprinted *bool `json:"fingerprinted,omitempty"`
 	// Challenged is the true if a challenge has occured
 	Challenged *bool `json:"challenged,omitempty"`
+	// AresTransStatus is the ares transaction status
+	AresTransStatus *string `json:"ares_trans_status,omitempty"`
+	// CresTransStatus is the cres transaction status
+	CresTransStatus *string `json:"cres_trans_status,omitempty"`
+	// DsTransID is the universally unique transaction identifier assigned by the DS to identify a single transaction
+	DsTransID *string `json:"ds_trans_id,omitempty"`
+	// FingerprintCompletionIndicator is the indicates whether the 3DS fingerprint successfully completed
+	FingerprintCompletionIndicator *string `json:"fingerprint_completion_indicator,omitempty"`
+	// ServerTransID is the universally unique transaction identifier assigned by the 3DS Server to identify a single transaction
+	ServerTransID *string `json:"server_trans_id,omitempty"`
 
 	client *ProcessOut
 }
@@ -47,6 +57,11 @@ func (s *ThreeDS) Prefill(c *ThreeDS) *ThreeDS {
 	s.Status = c.Status
 	s.Fingerprinted = c.Fingerprinted
 	s.Challenged = c.Challenged
+	s.AresTransStatus = c.AresTransStatus
+	s.CresTransStatus = c.CresTransStatus
+	s.DsTransID = c.DsTransID
+	s.FingerprintCompletionIndicator = c.FingerprintCompletionIndicator
+	s.ServerTransID = c.ServerTransID
 
 	return s
 }
